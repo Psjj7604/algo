565efe074b6c8c622b6df8c48b37c87ba728026d
seongjun <psjj7604@gmail.com>
2023-07-03 18:05:46 +0900

    dfs bfs - 적록색약 RGB로 나눠지는 구역 수 구하기

--- src/P10026.java
+++ src/P10026.java
@@ -7,7 +7,7 @@ import java.util.Queue;
 public class P10026 {
     static boolean[][] visited;
     static char[][] arr;
-    class Pair{
+    static class Pair{
         int x,y;
 
         Pair(int a,int b){
@@ -19,11 +19,14 @@ public class P10026 {
     static int[] dx = {0, 1, 0, -1};
     static int[] dy = {1, 0, -1, 0};
 
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         arr = new char[N][N];
         visited = new boolean[N][N];
+        int cnt1=0;
+        int cnt2=0;
 
         for (int i = 0; i < N; i++) {
             String st = br.readLine();
@@ -32,17 +35,41 @@ public class P10026 {
             }
         }
 
-        int cnt1 = 1;
-        int cnt2 = 1;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if(!visited[i][j]){
+                    bfs2(i,j);
+                    cnt1++;
+                }
+            }
+        }
+        visited = new boolean[N][N];
 
+        for (int i = 0; i < N; i++) {;
+            for (int j = 0; j < N; j++) {
+                if(arr[i][j]=='R')
+                    arr[i][j] = 'G';
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if(!visited[i][j]){
+                    bfs2(i,j);
+                    cnt2++;
+                }
+            }
+        }
+
+        System.out.println(cnt1+" "+cnt2);
 
     }
 
-    public void bfs1(int a, int b){
+    /*static void bfs1(int a, int b){
         Queue<Pair> queue = new LinkedList<>();
         queue.offer(new Pair(a, b));
-
         visited[a][b] = true;
+
         while(!queue.isEmpty()){
             Pair P = queue.poll();
             for (int i = 0; i < 4; i++) {
@@ -62,9 +89,9 @@ public class P10026 {
 
             }
         }
-    }
+    }*/
 
-    public void bfs2(int a, int b){
+    static void bfs2(int a, int b){
         Queue<Pair> queue = new LinkedList<>();
         queue.offer(new Pair(a, b));
 
